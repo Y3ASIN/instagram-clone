7d3634f912d0433738783cb081458d738e6c9478
Yeasin Arafat <yeasinarafat@Air.local>
2024-02-07 23:26:08 +0600

    front side of clone

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://yt3.googleusercontent.com/ytc/AIf8zZTDkajQxPa4sjDOW-c3er1szXkSAO-H9TiF4-8u_Q=s176-c-k-c0x00ffffff-no-rj"
+                class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freeCodeCamp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>617</strong> posts</div>
+                <div class="pe-5"><strong>157K</strong> followers</div>
+                <div class="pe-5"><strong>409</strong> following</div>
+            </div>
+            <div class="fw-bold pt-4">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together.
+                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+            <div>
+                <a href="#">freeCodeCamp.org</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fdac135-1.fna.fbcdn.net/v/t51.2885-15/418012074_756450603037651_3496036407781052045_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE3ODEuc2RyIn0&_nc_ht=instagram.fdac135-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=H4bd6yTM1a0AX-pY7mr&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzI3NzI5NzMyNzI4NTM5MDk0OA%3D%3D.2-ccb7-5&oh=00_AfAHTmdY6A-KL3c5hfPHE-8szr6iy1YfgkAnPAs0rq3p6Q&oe=65C6D72C&_nc_sid=ee9879"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fdac135-1.fna.fbcdn.net/v/t51.2885-15/419067373_1456049411628512_2173334813281754392_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE3OTAuc2RyIn0&_nc_ht=instagram.fdac135-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=JcCxL99ZjYgAX9SVWtu&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzI3ODc3OTg2NTQ5Njk1MDkxNQ%3D%3D.2-ccb7-5&oh=00_AfA4p__fe3Y3wiukvqCpvTTPuTu1YXSoWdf3A7IU4NpjeQ&oe=65C7A9C4&_nc_sid=ee9879"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fdac135-1.fna.fbcdn.net/v/t51.2885-15/419067373_1456049411628512_2173334813281754392_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE3OTAuc2RyIn0&_nc_ht=instagram.fdac135-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=JcCxL99ZjYgAX9SVWtu&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzI3ODc3OTg2NTQ5Njk1MDkxNQ%3D%3D.2-ccb7-5&oh=00_AfA4p__fe3Y3wiukvqCpvTTPuTu1YXSoWdf3A7IU4NpjeQ&oe=65C7A9C4&_nc_sid=ee9879"
+                class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
